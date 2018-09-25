7c9e43b1802fd508e0d24eab279f297354e523f5
taranu <dtaranu@astro.princeton.edu>
2018-09-25 14:09:15 -0500

    Updated analysis to use new models specs and fits

--- jupyternotebooks/cosmos_hst_analysis.py
+++ jupyternotebooks/cosmos_hst_analysis.py
@@ -13,6 +13,7 @@ import numpy as np
 import os
 
 import pandas as pd
+
 import pickle
 
 import seaborn as sns
@@ -24,6 +25,7 @@ import matplotlib.pyplot as plt
 #plt.style.use('seaborn-notebook')
 sns.set_style('darkgrid')
 mpl.rcParams['figure.dpi'] = 160
+mpl.rcParams['image.origin'] = 'lower'
 sns.set(rc={'axes.facecolor': '0.85', 'figure.facecolor': 'w'})
 
 
@@ -40,9 +42,9 @@ rgcfits = ap.io.fits.open(os.path.join(path, file))[1].data
 
 
 data = []
-
+ 
 files = glob.glob(os.path.expanduser(
-    "~/raid/lsst/cosmos/cosmos_25.2_fits_*_*.pickle.dat"))
+    "~/raid/lsst/cosmos/cosmos_25.2_fits_*_*_pickle.dat"))
 files.sort()
 for file in files:
     with open(file, 'rb') as f:
@@ -69,9 +71,9 @@ for file in files:
 #     SERSICFIT[7]: phi, the position angle in radians.  If phi=0, the major axis is
 #                   lined up with the x axis of the image.
 
-sources = ["hst"] + ["hst2hsc" + postfix for postfix in [
+sources = ["hst"] + ["_".join(["hst2hsc", postfix]) for postfix in [
 #    "",
-    "_ser",
+    "mgserbgepx",
 #    "_devexp"
 ]]
 params = {
@@ -84,27 +86,30 @@ idxparamscosmos = [[x + offset for x in [1, 2, 3, 7, 5, 6]] for offset in [0, 8]
 # profit was setup to output [cenx, ceny, flux, fluxrat, re, axrat, ang, nser]
 idxprofit = [2, 4, 7, 5, 6, 0, 1]
 idxprofittwo = [3, 4, 7, 5, 6, 0, 1, 8, 9, 12, 10, 11, 0, 1]
-idxprofitunlog = range(4)
-idxprofitunlogtwo = list(np.concatenate([np.arange(4), 7 + np.arange(4)]))
 idxparamsprofit = {
-    "gauss": (idxprofit, idxprofitunlog),
-    "multiexp": (idxprofit, idxprofitunlog),
-    "exp": (idxprofit, idxprofitunlog),
-    "multidev": (idxprofit, idxprofitunlog),
-    "dev": (idxprofit, idxprofitunlog),
-    "ser": (idxprofit, idxprofitunlog),
-    "cmodel": (idxprofittwo, idxprofitunlogtwo),
-    "devexp": (idxprofittwo, idxprofitunlogtwo),
+    "gausspx":     idxprofit,
+    "mgexppx":     idxprofit,
+#    "mgexpgpx": idxprofit,
+    "mgn2px":      idxprofit,
+    "mgdevepx":    idxprofit,
+    "mgserbgedpx": idxprofit,
+    "mgserbgepx":  idxprofit,
+    "serbpx":  idxprofit,
+    "serb":  idxprofit,
+    "mgcmodelpx":  idxprofittwo,
+    "mgdevexppx":  idxprofittwo,
+    "mgdevexpcpx": idxprofittwo,
 }
 paramsser = ["flux", "re", "n", "q", "phi", "x0", "y0"]
 models = {
     "single": {
         "cosmos": ["ser"],
-        "profit": ["gauss", "multiexp", "exp", "multidev", "dev", "ser"],
+        "profit": ["gausspx", "mgexppx", "mgn2px", "mgdevepx", 
+                   "mgserbgedpx", "mgserbgepx", "serbpx", "serb"],
     },
     "double": {
         "cosmos": ["devexp"],
-        "profit": ["cmodel", "devexp"],
+        "profit": ["mgcmodelpx", "mgdevexppx", "mgdevexpcpx"],
     },
 }
 modellers = {
@@ -124,7 +129,7 @@ colnames = (["id", "ra", "dec"] +
              for src in sources
              for model in idxparamsprofit.keys()
              for param in params["profit"]
-             for idx in range(1 + (model != "cmodel"))] +
+             for idx in range(1 + (model in models['double']['profit']))] +
             [".".join([prefix, x]) for prefix, colnames in colnames.items() for x in colnames])
 
 print(colnames)
@@ -141,6 +146,8 @@ indexmap = {ccat.getOrigIndex(i): i for i in range(ccat.getNObjects())}
 
 rows = []
 
+# Shouldn't have hardcoded this but here we are
+scaleratio = 0.168/0.03
 for datatab in data:
     appended = 0
     for idx in datatab:
@@ -151,35 +158,42 @@ for datatab in data:
             rec = ccat.getParametricRecord(indexmap[idx])
             row += [rec[param] for param in params["cosmos"]]
             for src in sources:
-                hasfits = hasfits and src in datatab[idx] 
+                hasfits = hasfits and src in datatab[idx]
                 if hasfits:
                     profit = datatab[idx][src]
                     hasfits = hasfits and 'fits' in profit
                     if hasfits:               
                         profit = profit['fits']
                         for model in idxparamsprofit:
-                            profitmodel = profit[model]['fits']['galsim']
-                            
-                            row += [profitmodelfit[param] for param in params["profit"] for profitmodelfit in profitmodel]
+                            hasfits = hasfits and model in profit['galsim']
+                            if hasfits:
+                                profitmodel = profit['galsim'][model]
+                                hasfits = hasfits and 'fits' in profitmodel
+                                if hasfits:
+                                    profitmodel = profitmodel['fits']
+                                    row += [profitmodelfit[param]
+                                            for param in params["profit"]
+                                            for profitmodelfit in profitmodel]
             if hasfits:
                 row += [rec["flux"][0]] + list(rec["sersicfit"][idxparamscosmos[0]])
                 for offset in range(2):
                     row += [rec["flux"][1+offset]] + list(rec["bulgefit"][idxparamscosmos[offset]])
                 for src in sources:
                     profit = datatab[idx][src]['fits']
-                    # The correct way to do this would be to take all of the values and undo the transformations
-                    # This is possible but for expediency I am not doing it
-                    for model, idxnser in idxparamsprofit.items():
-                        twocomp = model == "cmodel" or model == "devexp"
-                        values = np.array(profit[model]['fits']['galsim'][model != "cmodel"]["paramsbestall"])
-                        flux = 10**values[2] if twocomp else None
-                        values = values[idxnser[0]]
-                        values[idxnser[1]] = 10**values[idxnser[1]]
+                    for model, idxs in idxparamsprofit.items():
+                        twocomp = model in models['double']['profit']
+                        values = np.array(profit['galsim'][model]['fits'][-1]["paramsbestall"])
+                        if 'fluxscalehst2hsc' in datatab[idx][src]['metadata']:
+                            values[2] /= datatab[idx][src]['metadata'][
+                                'fluxscalehst2hsc']
+                            values[4] *= scaleratio
                         if twocomp:
-                            values[7] = values[7] - values[0]
-                            for col in [0, 7]:
-                                values[col] *= flux
-                        row += list(values)
+                            # Subtract first component fluxfrac
+                            values[8] = 1 - values[3]
+                            values[9] *= scaleratio
+                            for col in [3, 8]:
+                                values[col] *= values[2]
+                        row += list(values[idxs])
         if hasfits:
             rows.append(row)
             appended += 1
@@ -217,77 +231,95 @@ tab = pd.read_csv(os.path.join(path, "galfits.csv"))
 # Then HSC vs HST
 
 
-varnames = ["flux", "re", "n", "q"]
+varnames = ["flux", "re"]
 prefixes = [
-    ("profit.hst.ser", "cosmos.ser", varnames),
-    ("profit.hst.ser", "profit.hst2hsc_ser.ser", varnames),
+    ("profit.hst.serb", "cosmos.ser", varnames),
+    ("profit.hst.mgserbgedpx", "cosmos.ser", varnames),
+    ("profit.hst.mgserbgedpx", "profit.hst2hsc_mgserbgepx.mgserbgedpx", 
+     varnames),
 #    ("profit.hst.ser", "profit.hst2hsc_devexp.ser", vars),
 #    ("profit.hst.ser", "profit.hst2hsc.ser", vars)
 ]
 
+cmap = mpl.colors.ListedColormap(sns.color_palette("RdYlBu_r", 100))
+colors = (np.log10(tab["profit.hst.mgserbgedpx.n"])+1)/np.log10(60)
+colors = cmap(np.rint(100*colors)/100)
+
+
+def getname(var, prefix):
+    return ".".join([prefix, var])
+
+
 for prefixx, prefixy, varnames in prefixes:
-    ratios = {var: tab[".".join([prefixx, var])]/tab[".".join([prefixy,var])]
-              for var in varnames}
-    for i, x in enumerate(varnames):
+    ratios = {var: tab[getname(var, prefixx)]/tab[getname(var, prefixy)] for 
+              var in varnames}
+    for i, x in enumerate(varnames): 
         xlog = np.log10(ratios[x])
         xlog[xlog > 1] = 1
         xlog[xlog < -1] = -1
         for y in varnames[(i+1):(len(varnames)+1)]:
+            namey = getname(y, prefixy)
             ylog = np.log10(ratios[y])
             ylog[ylog > 1] = 1
             ylog[ylog < -1] = -1
-            fig = sns.jointplot(
-                x=xlog,
-                y=ylog,
-                color="k", joint_kws={'marker': '.', 's': 8},
-                marginal_kws={'hist_kws': {'log': True}},
-            ).set_axis_labels(
+            fig = sns.JointGrid(x=xlog, y=ylog)
+            fig.plot_joint(plt.scatter, c=colors, marker='.',
+                           edgecolor='k', s=24).set_axis_labels(
                 'log10({}_ratio) ({}/{})'.format(x, prefixx, prefixy),
                 'log10({}_ratio) ({}/{})'.format(y, prefixx, prefixy))
+            fig.plot_marginals(sns.distplot, kde=False, hist_kws={'log': True})
+
+        ylog = np.log10(tab[getname(x, prefixx)])
+        fig = sns.JointGrid(x=ylog, y=xlog)
+        fig.plot_joint(plt.scatter, c=colors, marker='.',
+                       edgecolor='k', s=24).set_axis_labels(
+            'log10({}) ({})'.format(x, prefixx),
+            'log10({}_ratio) ({}/{})'.format(x, prefixx, prefixy))
+        fig.plot_marginals(sns.distplot, kde=False, hist_kws={'log': True})
+        #cax = fig.add_axes([.94, .25, .02, .6])
 
 
-# In[9]:
+# In[ ]:
 
 
 # Which are the best models?
 modelsprofit = models["single"]["profit"] + models["double"]["profit"] 
 chisqredcols = {
     model: ".".join(["profit", "hst", model, "chisqred",
-                     "0" if model == "cmodel" else "1"]) 
+                     "1" if model in models['double']['profit'] else "0"]) 
     for model in modelsprofit
 }
+print(chisqredcols)
 modelbest = tab[list(chisqredcols.values())].idxmin(axis=1)
 modelbestcounts = modelbest.value_counts()
 print(modelbestcounts)
-for colx, coly in [("ser", "devexp"), ("ser", "cmodel"), ("devexp", "cmodel")]:
-    sns.jointplot(
-        x=np.log10(tab[chisqredcols[colx]]),
-        y=np.log10(tab[chisqredcols[colx]]/tab[chisqredcols[coly]]),
-        color="k", joint_kws={'marker': '.', 's': 8},
-        marginal_kws={'hist_kws': {'log': True}},
-    ).set_axis_labels(
+for colx, coly in [("mgserbgedpx", "mgdevexppx"), ("mgserbgedpx", "mgcmodelpx"), 
+                   ("mgdevexppx", "mgcmodelpx")]:
+    fig = sns.JointGrid(x=np.log10(tab[chisqredcols[colx]]),
+                        y=np.log10(tab[chisqredcols[colx]]/tab[chisqredcols[coly]]))
+    fig.plot_joint(plt.scatter, c=colors, marker='.',
+                   edgecolor='k', s=24).set_axis_labels(
         'log10(chisqred) ({})'.format(colx),
         'log10(chisqred) ({}/{})'.format(colx, coly))
 
 
-# In[10]:
+# In[17]:
 
 
 # Now compare only single-component models: Sersic vs best fixed n
-modelsfixedn = ["gauss", "exp", "dev"] 
+modelsfixedn = ["gausspx", "mgexppx", 'mgn2px', "mgdevepx"] 
 chisqredcolsfixedn = {
     model: ".".join(["profit", "hst", model, "chisqred",
-                     "0" if model == "cmodel" else "1"]) 
+                     "0" if model == "cmodel" else "0"]) 
     for model in modelsfixedn
 }
-print(list(chisqredcolsfixedn.values()))
 modelbest = tab[list(chisqredcolsfixedn.values())].idxmin(axis=1)
 print(modelbest.value_counts())
 # I seriously cannot figure out how to slice with modelbest
 # Surely there's a better way to do this?
 modelchisqmin = tab[list(chisqredcolsfixedn.values())].min(axis=1)
-colxname = "ser"
-labelbest = 'log10(chisqred) ({}/{})'.format(colxname, "best{gauss,exp,dev}")
+colxname = "mgserbgedpx"
+labelbest = 'log10(chisqred) ({}/{})'.format(colxname, "best{gauss,exp,n2,dev}")
 ratiobest = tab[chisqredcols[colxname]]/modelchisqmin
 # Plots:
 # How much better is Sersic than the best [gauss/exp/dev] vs how good is the 
@@ -296,16 +328,16 @@ ratiobest = tab[chisqredcols[colxname]]/modelchisqmin
 cols = [
     (tab[chisqredcols[colxname]], ratiobest,
      'log10(chisqred) ({})'.format(colxname), labelbest), 
-    (tab["profit.hst.ser.n"], ratiobest, 'log10(n_ser)', labelbest),
-    (tab["profit.hst.ser.n"],
-     tab[chisqredcols[colxname]]/tab[chisqredcols["exp"]],
+    (tab["profit.hst.mgserbgedpx.n"], ratiobest, 'log10(n_ser)', labelbest),
+    (tab["profit.hst.mgserbgedpx.n"],
+     tab[chisqredcols[colxname]]/tab[chisqredcols["mgexppx"]],
      'log10(n_ser)', 'log10(chisqred) ({}/exp)'.format(colxname)),
 ]
 for x, y, labelx, labely in cols:
     sns.jointplot(
         x=np.log10(x),
         y=np.log10(y),
-        color="k", joint_kws={'marker': '.', 's': 8},
+        color="k", joint_kws={'marker': '.', 's': 4},
         marginal_kws={'hist_kws': {'log': True}},
     ).set_axis_labels(labelx, labely)
 
