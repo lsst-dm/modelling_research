2f459bd84400cade0bed79cc2f302d08db98ea63
taranu <dtaranu@astro.princeton.edu>
2020-03-05 17:35:28 -0600

    Add time unit to time_print
    Also fill out docs.

--- timing.py
+++ timing.py
@@ -1,23 +1,32 @@
 from timeit import default_timer as timer
 
 
-def time_print(time, time_new=None, format_time='.1f', prefix='', postfix=''):
-    '''
+def time_print(time, time_new=None, format_time='.1f', prefix='', postfix='', unit='s'):
+    """ Print a message along with a time interval.
 
     Parameters
     ----------
-    time :
-    format_time
-    prefix
-    postfix
+    time : time-like
+        A previous time. Ignored if None.
+    time_new : time-like
+        A subsequent time of a compatible type as `time`. Default timeit.default_timer().
+    format_time : `str`
+        A string format for the time.
+    prefix : `str`
+        A string to prepend to the time; default empty.
+    postfix : `str`
+        A string to append to the time; default empty.
+    unit : `str`
+        The time unit to postfix to the timer output; default "s".
 
     Returns
     -------
-
-    '''
+    time_new : time-like
+        The subsequent time.
+    """
     if time_new is None:
         time_new = timer()
     timing = '' if time is None else (
-        f'{time_new-time:{format_time}}s' if (format_time is not None) else f'{time_new-time}s')
+        f'{time_new-time:{format_time}}{unit}' if (format_time is not None) else f'{time_new-time}{unit}')
     print(f'{prefix}{timing}{postfix}')
     return time_new
