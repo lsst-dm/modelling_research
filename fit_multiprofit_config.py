85da6601b17969f24918c0d621f909110a7aee4e
taranu <dtaranu@astro.princeton.edu>
2021-02-12 18:05:55 -0600

    Add example MultiProFitTask config

---
+++ fit_multiprofit_config.py
@@ -0,0 +1,12 @@
+from modelling_research.multiprofit_task import MultiProFitTask
+
+config.connections.name_output_cat = "multiprofit"
+config.fit_multiband.retarget(MultiProFitTask)
+config.fit_multiband.bands_read = ["r"]
+config.fit_multiband.fitCModelExp = False
+config.fit_multiband.fitSersicFromCModel = True
+config.fit_multiband.computeMeasModelfitLikelihood = True
+config.fit_multiband.usePriorShapeDefault = True
+config.fit_multiband.priorCentroidSigma = 0.2
+config.fit_multiband.priorMagBand = "r"
+config.fit_multiband.psfHwhmShrink = 0.1
